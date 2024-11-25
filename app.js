66ed5305319490ea7941e9001193bc0722cf4e22
aljersulit <aljeromesulit@gmail.com>
2024-11-25 19:19:31 +0800

    feat: Add function to get the random computer choice

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+	const randomNumber = Math.floor(Math.random() * 3) + 1;
+	switch (randomNumber) {
+		case 1:
+			return "Rock";
+		case 2:
+			return "Paper";
+		case 3:
+			return "Scissors";
+		default:
+			return "Something went wrong";
+	}
+}
+
+console.log(getComputerChoice());
+
