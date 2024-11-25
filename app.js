5b8d5512263f238f934a4ae5534b53ccfcbfafd5
aljersulit <aljeromesulit@gmail.com>
2024-11-25 19:48:44 +0800

    feat: Add function to get the player choice

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
 	const randomNumber = Math.floor(Math.random() * 3) + 1;
 	switch (randomNumber) {
 		case 1:
-			return "Rock";
+			return "rock";
 		case 2:
-			return "Paper";
+			return "paper";
 		case 3:
-			return "Scissors";
+			return "scissors";
 		default:
 			return "Something went wrong";
 	}
@@ -14,3 +14,25 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
+function getHumanChoice() {
+	let playerChoice = null;
+
+	while (!playerChoice) {
+		let lowerCaseUserInput = prompt(
+			"Please enter your choice(rock/paper/scissors):"
+		).toLowerCase();
+
+		switch (lowerCaseUserInput) {
+			case "rock":
+			case "paper":
+			case "scissors":
+				playerChoice = lowerCaseUserInput;
+			default:
+				continue;
+		}
+	}
+	return playerChoice;
+}
+
+
+console.log(getHumanChoice());
