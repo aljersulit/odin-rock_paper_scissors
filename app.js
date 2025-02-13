c413c9427db341be63e20cd496f3831607977eb7
Aldrin <aljeromesulit@gmail.com>
2025-02-13 20:04:00 +0800

    Refactor the game and add UI

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-// Start the game
-console.log(playGame());
+let humanScore = 0;
+let computerScore = 0;
+
+const result = document.getElementById("game-result");
+const humanScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const buttons = document.querySelectorAll("button.game-btn");
+const restart = document.getElementById("restart");
+
+buttons.forEach((button) => {
+	button.addEventListener("click", playRound);
+});
+
+restart.addEventListener("click", () => {
+	humanScore = 0;
+	humanScoreDisplay.textContent = humanScore;
+	computerScore = 0;
+	computerScoreDisplay.textContent = computerScore;
+	result.textContent = "";
+	buttons.forEach((button) => {
+		button.disabled = false;
+	});
+	restart.style.display = "none";
+});
 
 function getComputerChoice() {
 	const randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -15,107 +37,40 @@ function getComputerChoice() {
 	}
 }
 
-function getHumanChoice() {
-	// Wrapped in the while loop to prompt the user again whenever they typed an invalid input
-	while (true) {
-		// Get the user input
-		let userInput = prompt(
-			"Please enter your choice(rock/paper/scissors):"
-		);
-		// Make the user input case insensitive by forcing it to be lowercased to match the cases
-		let lowerCaseUserInput = "";
-		if (userInput) {
-			lowerCaseUserInput = userInput.toLowerCase();
-		}
-
-		// If the user clicks the cancel button on the prompt
-		if (userInput === null) {
-			return "cancel";
-		}
-
-		switch (lowerCaseUserInput) {
-			case "rock":
-			case "paper":
-			case "scissors":
-				return lowerCaseUserInput;
-			default:
-				alert("Wrong input");
-				continue;
-		}
-	}
-}
-
-function capitalize(text) {
-	return text.charAt(0).toUpperCase() + text.slice(1);
-}
-
-function playRound() {
-	let humanChoice = "";
-	let computerChoice = "";
-	let isDraw = true;
-	let winner = "";
-
-	while (isDraw) {
-		humanChoice = getHumanChoice();
-		computerChoice = getComputerChoice();
-
-		if (humanChoice === "cancel") {
-			return "stop";
-		}
-
-		if (humanChoice === computerChoice) {
-			alert("It's a draw, try again");
-			continue;
-		} else if (
-			(humanChoice === "rock" && computerChoice === "paper") ||
-			(humanChoice === "paper" && computerChoice === "scissors") ||
-			(humanChoice === "scissors" && computerChoice === "rock")
-		) {
-			winner = "computer";
-			isDraw = false;
-			console.log(
-				`You lose! ${capitalize(computerChoice)} beats ${capitalize(
-					humanChoice
-				)}`
-			);
-		} else {
-			winner = "human";
-			isDraw = false;
-			console.log(
-				`You win! ${capitalize(humanChoice)} beats ${capitalize(
-					computerChoice
-				)}`
-			);
-		}
-	}
-	return winner;
-}
-
-function playGame() {
-	let humanScore = 0;
-	let computerScore = 0;
-	const rounds = 5;
-
-	for (let round = 1; round <= rounds; round++) {
-		console.log(`Round: ${round}`);
-		let roundResult = playRound();
-		if (roundResult === "stop") {
-			return "Player stopped the game";
-		} else {
-			if (roundResult === "computer") {
-				computerScore++;
-			} else if (roundResult === "human") {
-				humanScore++;
-			}
-			console.log("Player score: ", humanScore);
-			console.log("Computer score:", computerScore);
-		}
-	}
-
-	if (humanScore > computerScore) {
-		return "You won. Congratulations!";
+function getWinner(humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) {
+		result.textContent = "It's a tie";
+	} else if (
+		(humanChoice === "rock" && computerChoice === "paper") ||
+		(humanChoice === "paper" && computerChoice === "scissors") ||
+		(humanChoice === "scissors" && computerChoice === "rock")
+	) {
+		result.textContent = "You Lose";
+		computerScore++;
+		computerScoreDisplay.textContent = computerScore;
 	} else {
-		return "You lose. Don't give up and try again";
+		result.textContent = "You Win";
+		humanScore++;
+		humanScoreDisplay.textContent = humanScore;
 	}
+	isGameOver();
+}
+
+function isGameOver() {
+	if ((humanScore >= 5) | (computerScore >= 5)) {
+		result.textContent =
+			humanScore > computerScore ? "You won the game!" : "You lost...";
+		buttons.forEach((button) => {
+			button.disabled = true;
+		});
+		restart.style.display = "block";
+	}
+}
+
+function playRound(event) {
+	const humanChoice = event.target.id;
+	const computerChoice = getComputerChoice();
+
+	getWinner(humanChoice, computerChoice);
 }
 
